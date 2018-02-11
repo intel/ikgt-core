44a9cb3024bd4c1037be9798173c9f85f63aa13d
Yan, Xiangyang <xiangyangx.yan@intel.com>
2018-02-11 15:48:35 +0800

    [VMM] keep tsc growing in Trusty side after S3
    this change is to prevent tsc reset after resuming back from S3.
    
    change detail:
    1. Before entering S3, save Trusty tsc (the tsc from Trusty's view)
    2. After resuming from S3, use TSC_OFFSET to adjust Trusty tsc
       to the saved value.
    
    Change-Id: I187a26f67cf307994e6e6755e4433eb96e7c399a
    Signed-off-by: Yan, Xiangyang <xiangyangx.yan@intel.com>

--- vmm/modules/tsc/tsc.c
+++ vmm/modules/tsc/tsc.c
@@ -45,7 +45,10 @@ static void tsc_write(guest_cpu_handle_t gcpu, UNUSED uint32_t msr_id)
 	guest_cpu_handle_t next_gcpu = gcpu->next_same_host_cpu;
 	uint64_t msr_value = get_val_for_wrmsr(gcpu);
 
-	offset_value = (asm_rdmsr(MSR_TIME_STAMP_COUNTER) - msr_value);
+	D(print_warn("%s() is called from gcpu(%d), msr value: 0x%llx -> 0x%llx\n", __FUNCTION__,
+				next_gcpu->id, asm_rdmsr(msr_id), msr_value));
+
+	offset_value = (asm_rdmsr(msr_id) - msr_value);
 	while (gcpu != next_gcpu)
 	{
 		if(next_gcpu->guest->id != 0)
@@ -58,22 +61,8 @@ static void tsc_write(guest_cpu_handle_t gcpu, UNUSED uint32_t msr_id)
 		next_gcpu = next_gcpu->next_same_host_cpu;
 	}
 
-	asm_wrmsr(MSR_TIME_STAMP_COUNTER, msr_value);
+	asm_wrmsr(msr_id, msr_value);
 	gcpu_skip_instruction(gcpu);
-}
-
-static void tsc_swap_in(guest_cpu_handle_t gcpu, UNUSED void *pv)
-{
-	uint64_t offset_value;
-	vmcs_obj_t vmcs = gcpu->vmcs;
-
-	if(gcpu->guest->id != 0)
-	{
-		/* In other guests, we adjust the VMCS_TSC_OFFSET to
-		 * keep the TSC unaffected from guest 0.*/
-		offset_value = -asm_rdmsr(MSR_TSC_ADJUST);
-		vmcs_write(vmcs, VMCS_TSC_OFFSET, offset_value);
-	}
 }
 
 static void tsc_setup(UNUSED guest_cpu_handle_t gcpu, void *pv)
@@ -102,6 +91,49 @@ static void tsc_gcpu_init(guest_cpu_handle_t gcpu, UNUSED void *pv)
 	}
 }
 
+#ifdef MODULE_SUSPEND
+
+/* for all non-guest0's gcpu, save its tsc (= hw tsc+VMCS_TSC_OFFSET) to VMCS_TSC_OFFSET */
+static void save_tsc_before_S3(guest_cpu_handle_t gcpu, UNUSED void *pv)
+{
+	guest_cpu_handle_t next_gcpu = gcpu->next_same_host_cpu;
+
+	while (gcpu != next_gcpu)
+	{
+		if(next_gcpu->guest->id != 0)
+		{
+			vmcs_write(next_gcpu->vmcs, VMCS_TSC_OFFSET,
+				(asm_rdtsc() + vmcs_read(next_gcpu->vmcs, VMCS_TSC_OFFSET)));
+			D(print_info("%s(): guest(%d) gcpu(%d) TSC_OFFSET=0x%llx\n", __FUNCTION__,
+				next_gcpu->guest->id, next_gcpu->id, vmcs_read(next_gcpu->vmcs, VMCS_TSC_OFFSET)));
+		}
+		next_gcpu = next_gcpu->next_same_host_cpu;
+	}
+}
+
+/* for all non-guest0's gcpu, restore its tsc
+ * before S3, guest_tsc = old_hw_tsc + VMCS_TSC_OFFSET. the guest_tsc is saved to VMCS_TSC_OFFSET
+ * in save_tsc_before_S3()
+ * So, the value in VMCS_TSC_OFFSET is the tsc expected in guest's view */
+static void restore_tsc_after_S3(guest_cpu_handle_t gcpu, UNUSED void *pv)
+{
+	guest_cpu_handle_t next_gcpu = gcpu->next_same_host_cpu;
+
+	while (gcpu != next_gcpu)
+	{
+		if(next_gcpu->guest->id != 0)
+		{
+			vmcs_write(next_gcpu->vmcs, VMCS_TSC_OFFSET,
+				(vmcs_read(next_gcpu->vmcs, VMCS_TSC_OFFSET)) - asm_rdtsc());
+			D(print_info("%s(): guest(%d) gcpu(%d) TSC_OFFSET=0x%llx\n", __FUNCTION__,
+				next_gcpu->guest->id, next_gcpu->id, vmcs_read(next_gcpu->vmcs, VMCS_TSC_OFFSET)));
+		}
+		next_gcpu = next_gcpu->next_same_host_cpu;
+	}
+}
+
+#endif
+
 void tsc_init(void)
 {
 	VMM_ASSERT_EX((get_proctl1_cap(NULL) & PROC_TSC_OFFSET),
@@ -112,10 +144,12 @@ void tsc_init(void)
 
 	if(is_tsc_adjust_supported())
 	{
-		event_register(EVENT_GCPU_SWAPIN, tsc_swap_in);
-	}else{
-		/* since tsc adjust is not supported, we need to trap wrmsr
-		 * to tsc to get the tsc delta.*/
-		monitor_msr_write(0, MSR_TIME_STAMP_COUNTER, tsc_write);
+		monitor_msr_write(0, MSR_TSC_ADJUST, tsc_write);
 	}
+	monitor_msr_write(0, MSR_TIME_STAMP_COUNTER, tsc_write);
+
+#ifdef MODULE_SUSPEND
+	event_register(EVENT_SUSPEND_TO_S3, save_tsc_before_S3);
+	event_register(EVENT_RESUME_FROM_S3, restore_tsc_after_S3);
+#endif
 }
