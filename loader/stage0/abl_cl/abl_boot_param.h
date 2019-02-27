c1e71a18204eac3f042f441b09b47c1d485051da
Qi Yadong <yadong.qi@intel.com>
2019-02-27 13:43:59 +0800

    [loader] fill device_sec_info from ABL command line
    Parse "ABL.svnseed" and "ABL.rpmb" from command line.
    Fill device_sec_info according to parsing result.
    Refactor abl_boot_param.c when parsing cmdline.
    
    Change-Id: I4fe47193fa496c842b99dd063054a0ce96c39b1b
    Signed-off-by: Qi Yadong <yadong.qi@intel.com>

--- loader/stage0/abl_cl/abl_boot_param.h
+++ loader/stage0/abl_cl/abl_boot_param.h
@@ -14,12 +14,31 @@
 
 /* arguments parsed from cmdline */
 typedef struct cmdline_params {
-	uint64_t cpu_num;
-	uint64_t bzImage_base;
-	uint64_t bzImage_size;
-	uint64_t initrd_base;
-	uint64_t initrd_size;
+	uint32_t cpu_num;
+	uint32_t bzImage_base;
+	uint32_t bzImage_size;
+	uint32_t initrd_base;
+	uint32_t initrd_size;
+	uint32_t svnseed_addr;
+	uint32_t rpmb_key_addr;
 } cmdline_params_t;
+
+#define ABL_SEED_LEN 32
+#define ABL_SEED_LIST_MAX 4
+typedef struct _abl_seed_info {
+	uint8_t svn;
+	uint8_t reserved[3];
+	uint8_t seed[ABL_SEED_LEN];
+} abl_seed_info_t;
+
+typedef struct abl_svnseed {
+	uint32_t size_of_this_struct;
+	uint32_t version;
+	uint32_t num_seeds;
+	abl_seed_info_t seed_list[ABL_SEED_LIST_MAX];
+} abl_svnseed_t;
+
+#define ABL_RPMB_KEY_LEN 32U
 
 boolean_t cmdline_parse(multiboot_info_t *mbi, cmdline_params_t *cmdline_param);
 #endif
