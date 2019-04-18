8b2361b2ada0b05133b137247abef8b4e258396f
Shirley Zhao <shirley.zhao@intel.com>
2019-04-18 16:25:49 +0800

    Initialize nested vt module framwork
    Change-Id: Idfb8d7a5443922b10ddb1f7182ac6657d9ad4a1b
    Signed-off-by: Shirley Zhao <shirley.zhao@intel.com>

---
+++ vmm/modules/nested_vt/nested_vt.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015-2019 Intel Corporation.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include "modules/nested_vt.h"
+
+void nested_vt_init(void)
+{
+}
